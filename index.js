5b40d06f5e8af13e0ed3aabfa71e4719091da208
aruYermek <yermek.aru@gmail.com>
2024-01-09 18:15:08 +0600

    install packages and connect with db

---
+++ index.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+// соединение к базе данных
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'assignment1_db',
+    password: 'mnbv01',
+    port: 5432,
+});
+
+// Пример запроса к базе данных
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Ошибка выполнения запроса:', err);
+    } else {
+        console.log('Результат запроса:', res.rows);
+    }
+    pool.end(); // Закрыть соединение после выполнения запроса
+});
+
+console.log(pool.query)
